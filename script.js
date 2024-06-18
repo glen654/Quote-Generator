ad0b1e46b0b3131ff77d227b02bda82fab482e85
Glen Alloy <glenalloy8@gmail.com>
2024-06-18 12:19:59 +0530

    delete console log and created new function to load quotes

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let apiQuotes = [];
 
+//Show new Quote
+function newQuote(){
+    
+}
+
 // Get Quotes From API
 async function getQuotes(){
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
        const response = await fetch(apiUrl);
        apiQuotes = await response.json();
-       console.log(apiQuotes); 
+       newQuote();
     } catch (error) {
         // Catch Error Here
     }
